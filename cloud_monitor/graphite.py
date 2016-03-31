83b9e63a268be35cc681cb8784350a835bf11156
Darryl Ring <darryl@darrylring.com>
2016-03-31 13:57:38 -0700

    Actually account for missing API data

--- cloud_monitor/graphite.py
+++ cloud_monitor/graphite.py
@@ -59,7 +59,8 @@ def summary():
                 cloud['hide'] = False
 
             if 'api-vms' in cloud:
-                cloud['lost'] = cloud['api-vms'].get('error', 0) + cloud['api-vms'].get('starting', 0) + cloud['api-vms'].get('running', 0) - sum([vmtype['total'] for vmtype in cloud['vms'].values()])
+                api_vms = [cloud['api-vms'].get('error', 0), cloud['api-vms'].get('starting', 0), cloud['api-vms'].get('running', 0)]
+                cloud['lost'] = sum(filter(None, api_vms)) - sum([vmtype['total'] for vmtype in cloud['vms'].values()])
                 if cloud['lost'] < 0:
                     cloud['lost'] = 0
 
